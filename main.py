1a559cc1d7d6f8b98a14b55c5a1afb2636f5ebe6
Filip Kozal <109867923+FilipK206@users.noreply.github.com>
2024-05-04 12:56:53 +0200

    Refactor code to address pylint insights
    - Removed trailing whitespace at the end of lines
    - Added a module-level docstring to describe the purpose of the module

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+"""
+This module contains a Flask application for temperature conversion and serves as the main entry point.
+"""
+
 import uuid
 from flask import Flask
 
 # Creating a Flask application instance
-app = Flask(__name__)  
+app = Flask(__name__)
 
 # Generating a Universally Unique Identifier (UUID) using the built-in uuid module
-app_id = uuid.uuid4()  
+app_id = uuid.uuid4()
 
 # Defining a home route
 @app.route("/")
@@ -17,24 +21,25 @@ def application_home():
     return {'app id': app_id}, 200
 
 # Defining a route for the temperature conversion with dynamic parameter
-@app.route("/convert/<fahrenheit>") 
+@app.route("/convert/<fahrenheit>")
 def temp_convert(fahrenheit):
     """
     Route handler for URL pattern "/convert/<fahrenheit>".
-    Converts Fahrenheit temperature to Celsius and returns the result along with the application ID.
+    Converts Fahrenheit temperature to Celsius and returns the result 
+    along with the application ID.
     """
     try:
-        fahrenheit = float(fahrenheit)  
+        fahrenheit = float(fahrenheit)
 
         # Calculating Celsius temperature
-        celsius = (fahrenheit - 32) * 5/9  
-        
+        celsius = (fahrenheit - 32) * 5/9
+
         # Returning a JSON response containing the Celsius temperature and the application ID
         return {'celsius': celsius, 'app id': app_id}, 200
-    
+
     except ValueError:
         # Handling the case where the input cannot be converted to a float
-        return {"Error": "Invalid input"}, 400 
+        return {"Error": "Invalid input"}, 400
 
 if __name__ == '__main__':
     app.run(debug=True)  # Running the Flask application in debug mode
