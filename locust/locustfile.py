879bb08d9fd11cbb80f4c0ed4b485167855a78ff
Filip Kozal <109867923+FilipK206@users.noreply.github.com>
2024-05-08 01:49:33 +0200

    Adds tags to the locust's tasks

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -5,10 +5,12 @@ class WebsiteUser(HttpUser):
     # Setting the wait time between requests
     wait_time = between(1, 3)
 
-    # @task
-    # def temperature_conversion(self):
-    #     self.client.get("/convert/50")
+    @tag('flask')
+    @task
+    def temperature_conversion(self):
+        self.client.get("/convert/50")
 
+    @tag('tf')
     @task
     def temperature_prediction(self):
         # Sending a POST request to the model's prediction endpoint
